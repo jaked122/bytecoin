b7d04cc258ddaaf24f85159172f72ff1b9c166c7
DavidVorick <david.vorick@gmail.com>
2013-10-06 09:29:39 +0000

    added some basic database support

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ The client takes the argument 'store' followed by a filename, opens a
 connection to the server on port 8080 and sends the file to the server.
 
 The server takes the file and prints it to storage.ytc
+
+In order to store the block chain, it was decided that a full database would
+be necessary. The database chosen is sqlite. For that reason, you will need to
+run the command: 'go get github.com/mattn/go-sqlite3'
+
+the database currently contains a single table of hosts and files. each file
+must be unique. each file has an associated host, multiple files can have
+the same host. The database is saved to a file for persistence. Currently hosts
+and files are always stored with the same key.
--- server.go
+++ server.go
@@ -4,9 +4,11 @@ import (
 	"os"
 	"net"
 	"fmt"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, db *sql.DB) {
 	fmt.Printf("Incoming Connection!\n")
 
 	outputFile, err := os.Create("storage.ytc")
@@ -21,21 +23,43 @@ func handleConnection(conn net.Conn) {
 		buf := make([]byte, 4096)
 		bytesRead, err := conn.Read(buf)
 		if err != nil {
-			return
+			break
 		}
 
-		n, err := outputFile.Write(buf[:bytesRead])
-		fmt.Printf("Bytes Written: %u\n", n);
+		_, err = outputFile.Write(buf[:bytesRead])
 		if err != nil {
 			fmt.Printf("Had problems writing the file!\n")
 		}
 	}
+
+	// We need to hash the file before putting it in the db
+	_, err = db.Exec("INSERT INTO hostsAndFiles(file, host) VALUES(25, 45)")
+	if err != nil {
+		fmt.Printf("Database query failed, most likely the file is already in the system\n")
+	} else {
+		fmt.Printf("Hopefully the db quere executed\n");
+	}
 }
 
 func main() {
+	// Try to get server online
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Printf("Server failed to open\n")
+		os.Exit(1)
+	}
+
+	// Try to open the database
+	db, err := sql.Open("sqlite3", "networkState.db")
+	if err != nil {
+		fmt.Printf("SQL Database failed to open!\n")
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS hostsAndFiles (file INT NOT NULL PRIMARY KEY, host INT)")
+	if err != nil {
+		fmt.Printf("Database Query Failed!\n")
+		os.Exit(1)
 	}
 
 	for {
@@ -46,6 +70,6 @@ func main() {
 		}
 		
 		// Should this be restful?
-		go handleConnection(conn)
+		go handleConnection(conn, db)
 	}
 }
