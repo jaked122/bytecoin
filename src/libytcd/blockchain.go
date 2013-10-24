0b219d8f553c2bb25d30f7123a3a1bed52d0b5a8
Colin Rice <colin@daedrum.net>
2013-10-23 23:29:19 -0400

    Pull out blockchain into its own file

--- src/libytcd/blockchain.go
+++ src/libytcd/blockchain.go
@@ -10,3 +10,21 @@ type Transaction struct {
 	Amount      YTCVolume
 	Signature   SignedTransaction
 }
+
+type BlockChain struct {
+	state        map[Account]YTCVolume
+	transactions []Transaction
+}
+
+func NewBlockChain() (b *BlockChain) {
+	b = new(BlockChain)
+	b.transactions = make([]Transaction, 100)
+	b.state = make(map[Account]YTCVolume)
+	return
+}
+
+func (b *BlockChain) AddTransaction(t Transaction) {
+	b.transactions = append(b.transactions, t)
+	b.state[t.Source] -= t.Amount
+	b.state[t.Destination] += t.Amount
+}
--- src/libytcd/server.go
+++ src/libytcd/server.go
@@ -15,23 +15,27 @@ const (
 )
 
 type ytcServer struct {
-	state        map[Account]YTCVolume
-	transactions []Transaction
+	b *BlockChain
+	d *http.ServeMux
 }
 
 func NewYtcd() (y *ytcServer) {
 	y = new(ytcServer)
-	y.transactions = make([]Transaction, 10)
-	y.state = make(map[Account]YTCVolume)
+	y.b = NewBlockChain()
+
+	y.d = http.NewServeMux()
+	y.d.HandleFunc(root, y.loadHomepage)
+	y.d.HandleFunc(postTransaction, y.handleTransaction)
+
 	return
 }
 
 // Will eventually load html explaining how to get started?
-func loadHomepage(w http.ResponseWriter, r *http.Request) {
+func (y *ytcServer) loadHomepage(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello, world")
 }
 
-func handleTransaction(w http.ResponseWriter, r *http.Request) {
+func (y *ytcServer) handleTransaction(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "learning how to parse http requests")
 
 	b, err := ioutil.ReadAll(r.Body)
@@ -39,8 +43,8 @@ func handleTransaction(w http.ResponseWriter, r *http.Request) {
 		log.Print(err)
 	}
 
-	v := new(Transaction)
-	err = json.Unmarshal(b, v)
+	t := new(Transaction)
+	err = json.Unmarshal(b, t)
 	if err != nil {
 		log.Print(err)
 	}
@@ -52,25 +56,14 @@ func handleTransaction(w http.ResponseWriter, r *http.Request) {
 	// t := {http request body}
 	// error checking on t
 	// verifying signature on t
-	// AddTransaction(t)
+	y.b.AddTransaction(*t)
 	// send non error response?
 }
 
-func (y *ytcServer) AddTransaction(t Transaction) {
-	y.transactions = append(y.transactions, t)
-	y.state[t.Source] -= t.Amount
-	y.state[t.Destination] += t.Amount
-}
-
 func (y *ytcServer) Listen(addr string) (err error) {
-
-	d := http.NewServeMux()
-	d.HandleFunc(root, loadHomepage)
-	d.HandleFunc(postTransaction, handleTransaction)
-
 	s := &http.Server{
 		Addr:           addr,
-		Handler:        d,
+		Handler:        y.d,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
