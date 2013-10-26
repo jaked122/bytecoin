6222f2bdf0b23de5d725f865b06346a7ce98c7e3
Colin Rice <colin@daedrum.net>
2013-10-26 19:15:01 -0400

    libytcd: Seperate Network & Control Listening
    Note that we do some sketchy double json parsing

--- src/libytcd/server.go
+++ src/libytcd/server.go
@@ -1,7 +1,9 @@
 package libytcd
 
 import (
+	"encoding/json"
 	"io"
+	"net"
 	"net/http"
 	"time"
 )
@@ -9,6 +11,7 @@ import (
 const (
 	root            = "/"
 	postTransaction = "/postTransaction"
+	newWallet       = "/newWallet"
 )
 
 type ytcServer struct {
@@ -22,7 +25,7 @@ func NewYtcd() (y *ytcServer) {
 
 	y.d = http.NewServeMux()
 	y.d.HandleFunc(root, y.loadHomepage)
-	y.d.HandleFunc(postTransaction, y.handleTransaction)
+	y.d.HandleFunc(newWallet, y.newWallet)
 
 	return
 }
@@ -32,12 +35,47 @@ func (y *ytcServer) loadHomepage(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello, world")
 }
 
-func (y *ytcServer) handleTransaction(w http.ResponseWriter, r *http.Request) {
+func (y *ytcServer) newWallet(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "learning how to parse http requests")
 
 }
 
-func (y *ytcServer) Listen(addr string) (err error) {
+func (y *ytcServer) HandleNetworkConnection(c net.Conn) {
+	j := json.NewDecoder(c)
+	for {
+		var v interface{}
+		j.Decode(v)
+
+		b, err := json.Marshal(v)
+		if err != nil {
+			break
+		}
+
+		update, err := ParseUpdate(b)
+		if err != nil {
+			break
+		}
+		update.Verify(y.s)
+		update.Apply(y.s)
+	}
+}
+
+func (y *ytcServer) ListenNetwork(addr string) (err error) {
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			return err
+		}
+		go y.HandleNetworkConnection(c)
+	}
+}
+
+func (y *ytcServer) ListenCtl(addr string) (err error) {
 	s := &http.Server{
 		Addr:           addr,
 		Handler:        y.d,
--- src/libytcd/server_test.go
+++ src/libytcd/server_test.go
@@ -7,7 +7,7 @@ import (
 func TestSimple(t *testing.T) {
 	y := NewYtcd()
 	go func() {
-		err := y.Listen(":1337")
+		err := y.ListenCtl(":1337")
 		t.Fatal(err)
 	}()
 }
